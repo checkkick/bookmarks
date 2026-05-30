f9a1f64e25f8b31d66d0025b20e7deb16764ad82
Konstantin <tuskancik226@gmail.com>
2026-05-30 19:35:19 +0300

    feat: add register form

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     # path("login/", views.user_login, name="login"),
     path("", include("django.contrib.auth.urls")),
-    path("", views.dashboard, name="dashboard"),
     # path("login/", auth_views.LoginView.as_view(), name="login"),
     # path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     # path(
@@ -38,4 +37,6 @@ urlpatterns = [
     #     auth_views.PasswordResetCompleteView.as_view(),
     #     name="password_reset_complete",
     # ),
+    path("", views.dashboard, name="dashboard"),
+    path("register/", views.register, name="register"),
 ]
