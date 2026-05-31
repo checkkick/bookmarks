432e00f104451bc79b327cf649931a7f0430d5a9
Konstantin <tuskancik226@gmail.com>
2026-05-31 14:41:38 +0300

    feat: add media and edit profile

--- account/urls.py
+++ account/urls.py
@@ -39,4 +39,5 @@ urlpatterns = [
     # ),
     path("", views.dashboard, name="dashboard"),
     path("register/", views.register, name="register"),
+    path("edit/", views.edit, name="edit"),
 ]
--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,8 @@ LOGIN_REDIRECT_URL = "dashboard"
 LOGIN_URL = "login"
 LOGOUT_URL = "logout"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Application definition
 
